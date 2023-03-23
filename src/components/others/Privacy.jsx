0701ff4f53721a5c34eab6aeb4dbebc87a9fe886
GerardGhub <singian28@gmail.com>
2023-03-23 22:25:43 +0800

    Implement All Placeholder in Privacy Setup

--- src/components/others/Privacy.jsx
+++ src/components/others/Privacy.jsx
@@ -1,7 +1,35 @@
 import React, { Component, Fragment } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import AppURL from "../../api/AppURL";
+import axios from "axios";
 
 class Privacy extends Component {
+  constructor() {
+    super();
+    this.state = {
+      privacy: "",
+      loaderDiv: "",
+      mainDiv: "d-none",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(AppURL.AllSiteInfo)
+      .then((response) => {
+        let StatusCode = response.status;
+        if (StatusCode == 200) {
+          let JsonData = response.data[0]["privacy"];
+          this.setState({
+            privacy: JsonData,
+            loaderDiv: "d-none",
+            mainDiv: "",
+          });
+        }
+      })
+      .catch((error) => {});
+  }
+
   render() {
     return (
       <Fragment>
@@ -14,22 +42,42 @@ class Privacy extends Component {
               sm={12}
               xs={12}
             >
-              <h4 className="section-title-login">Privacy Page </h4>
-              <p className="section-title-contact">
-                I started to learn how to code when I was a 2nd year college , at my
-                first job as IT Specialist I experience all the different type
-                of MIS work such as Networking , CCTV Configuration, Graphic
-                Designing, Server Configuration, Hardware & Software
-                troubleshooting System Support for the existing system and
-                client concern for their technical concern aside of that we
-                maintain our existing system as a programmer and Developer for
-                the new system, after all I decided to chose software
-                development for my career growth programming gave me happiness
-                specially on successfully debugging a bug. during my journey as
-                software developer I invest a time on learning a full stack
-                development using the latest technologies. and also I'm dreaming
-                to work on It Industry
-              </p>
+              <div className={this.state.loaderDiv}>
+                <div class="ph-item">
+                  <div class="ph-col-12">
+                    <div class="ph-row">
+                      <div class="ph-col-4"></div>
+                      <div class="ph-col-8 empty"></div>
+                      <div class="ph-col-6"></div>
+                      <div class="ph-col-6 empty"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="ph-item">
+                  <div class="ph-col-12">
+                    <div class="ph-row">
+                      <div class="ph-col-4"></div>
+                      <div class="ph-col-8 empty"></div>
+                      <div class="ph-col-6"></div>
+                      <div class="ph-col-6 empty"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                      <div class="ph-col-12"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className={this.state.mainDiv}>
+                <h4 className="section-title-login">Privacy Page </h4>
+                <p className="section-title-contact">{this.state.privacy}</p>
+              </div>
             </Col>
           </Row>
         </Container>
@@ -39,4 +87,3 @@ class Privacy extends Component {
 }
 
 export default Privacy;
-
