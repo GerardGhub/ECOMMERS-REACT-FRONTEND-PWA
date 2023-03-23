5afac243fa1701462ddd4d130fa4807ebecc5d39
GerardGhub <singian28@gmail.com>
2023-03-23 16:22:58 +0800

    installing html parser

--- src/components/others/About.jsx
+++ src/components/others/About.jsx
@@ -1,7 +1,34 @@
 import React, { Component, Fragment } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import AppURL from "../../api/AppURL";
+import axios from "axios";
+// import ReactHtmlParser from 'react-html-parser';
+
 
 class About extends Component {
+  constructor() {
+    super();
+    this.state = {
+      about: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(AppURL.AllSiteInfo)
+      .then((response) => {
+        let StatusCode = response.status;
+        if (StatusCode == 200) {
+          let JsonData = response.data[0]["about"];
+          // console.log(response.data[0]['about']);
+          this.setState({ about: JsonData });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <Fragment>
@@ -16,20 +43,8 @@ class About extends Component {
             >
               <h4 className="section-title-login">About Us Page </h4>
               <p className="section-title-contact">
-                I started to learn how to code when I was a 2nd year college ,
-                at my first job as IT Specialist I experience all the different
-                type of MIS work such as Networking , CCTV Configuration,
-                Graphic Designing, Server Configuration, Hardware & Software
-                troubleshooting System Support for the existing system and
-                client concern for their technical concern aside of that we
-                maintain our existing system as a programmer and Developer for
-                the new system, after all I decided to chose software
-                development for my career growth programming gave me happiness
-                specially on successfully debugging a bug. during my journey as
-                software developer I invest a time on learning a full stack
-                development using the latest technologies. and also I'm dreaming
-                to work on It Industry
-              </p>
+              {/* { ReactHtmlParser(html) } */}
+                {this.state.about}</p>
             </Col>
           </Row>
         </Container>
